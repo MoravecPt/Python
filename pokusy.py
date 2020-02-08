28553e9e80effd4043da815eb614010b9ade00c9
Petr Moravec <moravec.pt@seznam.cz>
2020-02-08 21:33:21 +0100

    zkousky z w3

---
+++ pokusy.py
@@ -0,0 +1,13 @@
+python_obj = json.loads(json_obj)
+print("\nJSON data:")
+print(python_obj)
+print("\nName: ",python_obj["Name"])
+print("Class: ",python_obj["Class"])
+print("Age: ",python_obj["Age"])
+
+
+
+
+
+
+
